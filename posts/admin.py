178bc73b1e3fd5d04755c27f2f816cc3fcf57cdc
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-10-12 19:46:41 +0300

    posts/admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display =['author','content_preview','created_at']
+    list_filter =['created_at' ,'author']
+    search_fields =['content','author__username']
+    
+    def  content_preview(self,obj):
+        return obj.content[:50] + '- - -' if len (obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content'
 
 # Register your models here.
