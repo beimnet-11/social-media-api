8983ed4a2767bf3882c134e347479ccdce368e07
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-10-12 19:08:46 +0300

    post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import User
+class Post(models.Model):
+    author=models.ForeignKey(User,on_delete=models. CASCADE,  related_name ='posts')
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"post by{self.author.username} - {self.created_at}"
+
+    class Meta:
+        ordering = ['-created_at']
+
 
 # Create your models here.
