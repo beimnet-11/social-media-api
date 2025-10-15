649f14c16ec265b6391909b75490d3081e175db9
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-10-15 19:24:23 +0300

    user/views.py

--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny ,IsAuthenticated
+from rest_framework.authtoken.models import Token
+from.models import User
+from.serializers import UserRegistrationSerailizer , UserLoginSerailizer
 
-# Create your views here.
+@api_view(['Post'])
+@permission_classes([AllowAny])
+def login_user(request):
+    if request.method == 'post'
+        serializer=UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.validated_data['user']
+            token,created = token.objects.get_or_create(user=user)
+            return Response({
+                'message':'Login seccessful',
+                'user':{
+                    'id':user.id,
+                    'username':user.username,
+                    'email':user.email
+                    },
+                    'token':token.key
+           },status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
