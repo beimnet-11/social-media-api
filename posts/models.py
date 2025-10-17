8cded9b93e6ce58d3025422215b8180d5cf658f1
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-10-17 14:28:07 +0300

    posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-from user.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class Post(models.Model):
-    author=models.ForeignKey(User,on_delete=models. CASCADE,  related_name ='posts')
+    author=models.ForeignKey(User,on_delete=models.CASCADE,  related_name ='posts')
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"post by{self.author.username} - {self.created_at}"
+        return f"post by {self.author.username} - {self.created_at}"
 
     class Meta:
         ordering = ['-created_at']
-class like(models.Model):
-    user=models.ForeignKey(User,on_delete=models. CASCADE, related_name='likes')
-    post=models.ForeignKey(Post,on_delete=models. CASCADE, related_name='likes')
+class Like(models.Model):
+    user=models.ForeignKey(User,on_delete=models. CASCADE, related_name='Likes')
+    post=models.ForeignKey(Post,on_delete=models. CASCADE, related_name='Likes')
     created_at=models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -21,10 +22,12 @@ class like(models.Model):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.user.username} likes post {self.post.id}"    
+        return f"{self.user.username} likes post {self.post.id}"
+
+
 class Comment(models.Model):
-    user=models.ForeignKey(User,on_delete=models. CASCADE, related_name='comments')
-    post=models.ForeignKey(Post,on_delete=models. CASCADE, related_name='comments')
+    user=models.ForeignKey(User,on_delete=models.CASCADE, related_name='comments')
+    post=models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
