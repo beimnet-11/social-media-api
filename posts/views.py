b529bc5e5e4e52fae150a69b4376763836288c3f
beimnet-11 <beimnet.mi1pa@gmail.com>
2025-10-17 10:12:30 +0300

    posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post, like, Comment
+from .serializers import PostSerializer, PostCreateSerializer, CommentSerializer, CommentCreateSerializer, LikeSerializer
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def posts_list_create(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(author=request.user)
+            full_serializer = PostSerializer(post, context={'request': request})
+            return Response(full_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = PostSerializer(post, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        if post.author != request.user:
+            return Response({'error': 'You do not have permission to edit this post'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = PostCreateSerializer(post, data=request.data , partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            full_serializer = PostSerializer(post, context={'request': request})
+            return Response(full_serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        if post.author != request.user:
+            return Response({'error': 'You do not have permission to delete this post'}, status=status.HTTP_403_FORBIDDEN)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+@api_view(['POST','DELETE'])
+@permission_classes([IsAuthenticated])
+def like_post(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'POST':
+        like_instance, created = like.objects.get_or_create(user=request.user, post=post)
+        if created:
+            serializer = LikeSerializer(like_instance)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'You have already liked this post'}, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        try:
+            like_instance = like.objects.get(user=request.user, post=post)
+            like_instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except like.DoesNotExist:
+            return Response({'error': 'You have not liked this post'}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def comments_list_create(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            comment = serializer.save(user=request.user, post=post)
+            full_serializer = CommentSerializer(comment)
+            return Response(full_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)            
